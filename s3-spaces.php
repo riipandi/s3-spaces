36e67583b81a79d568847bfb0ebe1d44f3044cda
Tim Ruhay <timruhay@gmail.com>
2019-01-01 01:59:02 +0700

    Some changes

--- s3-spaces.php
+++ s3-spaces.php
@@ -17,12 +17,13 @@ require dirname(__FILE__) . DIRECTORY_SEPARATOR . 's3spaces_class_filesystem.php
 load_plugin_textdomain('s3spaces', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 // Add settings link
-$plugin = plugin_basename( __FILE__ );
 function s3spaces_add_settings_link( $links ) {
-    $settings_link = '<a href="options-general.php?page='.$plugin.'">' . __( 'Settings' ) . '</a>';
+    $settings_link = '<a href="options-general.php?page=s3-spaces">' . __( 'Settings' ) . '</a>';
     array_push( $links, $settings_link );
   	return $links;
 }
+
+$plugin = plugin_basename( __FILE__ );
 add_filter( "plugin_action_links_$plugin", 's3spaces_add_settings_link' );
 
 function s3spaces_incompatibile($msg) {
